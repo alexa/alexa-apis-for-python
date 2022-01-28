85a629e9b7270a356c4f2fe428c475febd8c15b9
pranotipp <93741216+pranotipp@users.noreply.github.com>
2022-01-27 19:39:31 -0800

    Release 1.33.1. For changelog, check CHANGELOG.rst
    * Release 1.33.1. For changelog, check CHANGELOG.rst
    
    * fix: add missing changelog.rst files
    
    Co-authored-by: pranoti2 <pranoti210@gmail.com>

--- ask-sdk-model/ask_sdk_model/context.py
+++ ask-sdk-model/ask_sdk_model/context.py
@@ -28,6 +28,7 @@ if typing.TYPE_CHECKING:
     from ask_sdk_model.interfaces.viewport.typed_viewport_state import TypedViewportState as TypedViewportState_c366f13e
     from ask_sdk_model.interfaces.audioplayer.audio_player_state import AudioPlayerState as AudioPlayerState_ac652451
     from ask_sdk_model.interfaces.automotive.automotive_state import AutomotiveState as AutomotiveState_2b614eea
+    from ask_sdk_model.interfaces.alexa.experimentation.experimentation_state import ExperimentationState as ExperimentationState_37bb7c62
     from ask_sdk_model.interfaces.alexa.extension.extensions_state import ExtensionsState as ExtensionsState_f02207d3
     from ask_sdk_model.interfaces.applink.app_link_state import AppLinkState as AppLinkState_370eda23
     from ask_sdk_model.interfaces.geolocation.geolocation_state import GeolocationState as GeolocationState_5225020d
@@ -58,6 +59,8 @@ class Context(object):
     :type extensions: (optional) ask_sdk_model.interfaces.alexa.extension.extensions_state.ExtensionsState
     :param app_link: Provides the current state for app link capability.
     :type app_link: (optional) ask_sdk_model.interfaces.applink.app_link_state.AppLinkState
+    :param experimentation: Provides the current experimentation state
+    :type experimentation: (optional) ask_sdk_model.interfaces.alexa.experimentation.experimentation_state.ExperimentationState
 
     """
     deserialized_types = {
@@ -70,7 +73,8 @@ class Context(object):
         'viewport': 'ask_sdk_model.interfaces.viewport.viewport_state.ViewportState',
         'viewports': 'list[ask_sdk_model.interfaces.viewport.typed_viewport_state.TypedViewportState]',
         'extensions': 'ask_sdk_model.interfaces.alexa.extension.extensions_state.ExtensionsState',
-        'app_link': 'ask_sdk_model.interfaces.applink.app_link_state.AppLinkState'
+        'app_link': 'ask_sdk_model.interfaces.applink.app_link_state.AppLinkState',
+        'experimentation': 'ask_sdk_model.interfaces.alexa.experimentation.experimentation_state.ExperimentationState'
     }  # type: Dict
 
     attribute_map = {
@@ -83,12 +87,13 @@ class Context(object):
         'viewport': 'Viewport',
         'viewports': 'Viewports',
         'extensions': 'Extensions',
-        'app_link': 'AppLink'
+        'app_link': 'AppLink',
+        'experimentation': 'Experimentation'
     }  # type: Dict
     supports_multiple_types = False
 
-    def __init__(self, system=None, alexa_presentation_apl=None, audio_player=None, automotive=None, display=None, geolocation=None, viewport=None, viewports=None, extensions=None, app_link=None):
-        # type: (Optional[SystemState_22fcb230], Optional[RenderedDocumentState_4fad8b14], Optional[AudioPlayerState_ac652451], Optional[AutomotiveState_2b614eea], Optional[DisplayState_726e4959], Optional[GeolocationState_5225020d], Optional[ViewportState_a05eceb9], Optional[List[TypedViewportState_c366f13e]], Optional[ExtensionsState_f02207d3], Optional[AppLinkState_370eda23]) -> None
+    def __init__(self, system=None, alexa_presentation_apl=None, audio_player=None, automotive=None, display=None, geolocation=None, viewport=None, viewports=None, extensions=None, app_link=None, experimentation=None):
+        # type: (Optional[SystemState_22fcb230], Optional[RenderedDocumentState_4fad8b14], Optional[AudioPlayerState_ac652451], Optional[AutomotiveState_2b614eea], Optional[DisplayState_726e4959], Optional[GeolocationState_5225020d], Optional[ViewportState_a05eceb9], Optional[List[TypedViewportState_c366f13e]], Optional[ExtensionsState_f02207d3], Optional[AppLinkState_370eda23], Optional[ExperimentationState_37bb7c62]) -> None
         """
 
         :param system: Provides information about the current state of the Alexa service and the device interacting with your skill.
@@ -111,6 +116,8 @@ class Context(object):
         :type extensions: (optional) ask_sdk_model.interfaces.alexa.extension.extensions_state.ExtensionsState
         :param app_link: Provides the current state for app link capability.
         :type app_link: (optional) ask_sdk_model.interfaces.applink.app_link_state.AppLinkState
+        :param experimentation: Provides the current experimentation state
+        :type experimentation: (optional) ask_sdk_model.interfaces.alexa.experimentation.experimentation_state.ExperimentationState
         """
         self.__discriminator_value = None  # type: str
 
@@ -124,6 +131,7 @@ class Context(object):
         self.viewports = viewports
         self.extensions = extensions
         self.app_link = app_link
+        self.experimentation = experimentation
 
     def to_dict(self):
         # type: () -> Dict[str, object]
